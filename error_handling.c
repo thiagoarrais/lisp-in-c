aed35b1c50c02d54f98689d1a0d2dd1136b0cf7f
eduardo apolinario <eduardoapolinario@eduardos-MBP.home>
2016-02-27 19:08:53 -0800

    support for doubles - only addition for now

--- error_handling.c
+++ error_handling.c
@@ -5,13 +5,21 @@
 /* Enum of possible Error Types */
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
-enum { LVAL_NUM, LVAL_ERR };
+enum { LVAL_NUM, LVAL_DOUBLE, LVAL_ERR };
 
 typedef struct {
   int type;
   long num;
+  double numd;
   int err;
 } lval;
+
+lval lval_double(double x) {
+  lval v;
+  v.type = LVAL_DOUBLE;
+  v.numd = x;
+  return v;
+}
 
 lval lval_num(long x) {
   lval v;
@@ -29,6 +37,9 @@ lval lval_err(long x) {
 
 void lval_print(lval v) {
   switch (v.type) {
+    case LVAL_DOUBLE:
+      printf("%f", v.numd);
+      break;
     case LVAL_NUM:
       printf("%li", v.num);
       break;
@@ -60,7 +71,15 @@ lval eval_op(lval x, char* op, lval y) {
   }
 
   if (strcmp(op, "+") == 0) {
-    return lval_num(x.num + y.num);
+    if (x.type == y.type && x.type == LVAL_NUM) {
+      return lval_num(x.num + y.num);
+    } else if (x.type == LVAL_DOUBLE) {
+      return lval_double(x.numd + y.num);
+    } else if (y.type == LVAL_DOUBLE) {
+      return lval_double(x.num + y.numd);
+    } else {
+      return lval_num(x.num + y.num);
+    }
   }
   if (strcmp(op, "-") == 0) {
     return lval_num(x.num - y.num);
@@ -75,6 +94,13 @@ lval eval_op(lval x, char* op, lval y) {
       return lval_num(x.num / y.num);
     }
   }
+  /* if (strcmp(op, "%") == 0) { */
+  /*   if (y.num == 0) { */
+  /*     return lval_err(LERR_DIV_ZERO); */
+  /*   } else { */
+  /*     return lval_num(x.num % y.num); */
+  /*   } */
+  /* } */
 
   return lval_err(LERR_BAD_OP);
 }
@@ -82,8 +108,13 @@ lval eval_op(lval x, char* op, lval y) {
 lval eval(mpc_ast_t* t) {
   if (strstr(t->tag, "number")) {
     errno = 0;
-    long x = strtol(t->contents, NULL, 10);
-    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    if (strstr(t->contents, ".")) {
+      double d = strtod(t->contents, NULL);
+      return errno != ERANGE ? lval_double(d) : lval_err(LERR_BAD_NUM);
+    } else {
+      long x = strtol(t->contents, NULL, 10);
+      return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    }
   }
 
   char *op = t->children[1]->contents;
@@ -100,18 +131,19 @@ lval eval(mpc_ast_t* t) {
 
 int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Double = mpc_new("double");
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr = mpc_new("expr");
   mpc_parser_t* Lispy = mpc_new("lispy");
 
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
-      number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' ;                  \
+      number   : /-?[0-9]+\\.?[0-9]*/ ;                   \
+      operator : '+' | '-' | '*' | '/' | '%' ;            \
       expr     : <number> | '(' <operator> <expr>+ ')' ;  \
       lispy    : /^/ <operator> <expr>+ /$/ ;             \
     ",
-    Number, Operator, Expr, Lispy);
+    Double, Number, Operator, Expr, Lispy);
 
   puts("Lispy Version 0.0.0.0.4");
   puts("Press Ctrl+c to Exit \n");
@@ -133,7 +165,7 @@ int main(int argc, char** argv) {
     free(input);
   }
 
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  mpc_cleanup(5, Double, Number, Operator, Expr, Lispy);
 
   return 0;
 }
